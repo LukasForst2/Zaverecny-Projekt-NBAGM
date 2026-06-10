84f9cf7c5f06ad39084feb175e414609e15c809a
Lukáš <forstlukas245@student.voskh.cz>
2026-06-10 23:37:02 +0200

    Propojeni js a html

--- web.js
+++ web.js
@@ -23,10 +23,59 @@ if (teamBnameInp && teamBhead) {
 //Vyhledavac hracu, seznam
 const players = activePlayers;
 const playerList = document.getElementById(`playerListUl`);
-if (playerList) {
-    players.forEach((p) => {
-        const li = document.createElement(`li`);
-        li.textContent = `${p.fullName} - $${p.salary.toLocaleString()}`;
-        playerList.appendChild(li);
-    });
+const filterGuard = document.getElementById('filterGuard');
+const filterWing = document.getElementById('filterWing');
+const filterBig = document.getElementById('filterBig');
+function loadPlayerList() {
+    if (playerList) {
+        playerList.innerHTML = '';
+        //check na zaskrtnute pozice ve vyhledavaci
+        const allowedPos = [];
+        if (filterGuard.checked)
+            allowedPos.push("Guard");
+        if (filterWing.checked)
+            allowedPos.push("Wing");
+        if (filterBig.checked)
+            allowedPos.push("BigMan");
+        const filteredPlayers = players.filter(p => allowedPos.includes(p.position));
+        filteredPlayers.forEach((p) => {
+            const li = document.createElement(`li`);
+            //prvni sloupec jmeno
+            const nameCol = document.createElement(`span`);
+            nameCol.textContent = p.fullName;
+            //druhy sloupec pozice
+            const posCol = document.createElement(`span`);
+            posCol.textContent = p.position;
+            //treti sloupec overall
+            const ovrCol = document.createElement(`span`);
+            ovrCol.textContent = `OVR: ${p.calcOverall().toString()}`;
+            //ctvrty sloupec, plat
+            const slryCol = document.createElement(`span`);
+            slryCol.textContent = `SAL: ${(p.salary / 1000000).toFixed(0)}M`;
+            //paty sloupec tlacitka na pridani do tymu A/B
+            const butCont = document.createElement(`div`);
+            butCont.className = `add-btn`;
+            const btnA = document.createElement(`button`);
+            btnA.textContent = `+A`;
+            btnA.addEventListener(`click`, () => {
+            });
+            const btnB = document.createElement(`button`);
+            btnB.textContent = `+B`;
+            btnB.addEventListener(`click`, () => {
+            });
+            //sestaveni karty hrace ve vyhledavaci
+            butCont.appendChild(btnA);
+            butCont.appendChild(btnB);
+            li.appendChild(nameCol);
+            li.appendChild(posCol);
+            li.appendChild(ovrCol);
+            li.appendChild(slryCol);
+            li.appendChild(butCont);
+            playerList.appendChild(li);
+        });
+    }
 }
+filterGuard.addEventListener('change', loadPlayerList);
+filterWing.addEventListener('change', loadPlayerList);
+filterBig.addEventListener('change', loadPlayerList);
+loadPlayerList();
